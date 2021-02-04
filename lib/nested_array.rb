ba29bba6eb1180e36c1830805d75c034692fb344
Brian Cook <brybcook@gmail.com>
2021-02-04 19:21:39 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,7 +32,8 @@ def sorted_matrix
   # uses the arrays of conventional and organic produce as before.
   # However, this time, sort each internal array alphabetically by the first character
   array.sort[
-    ]
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE]
 end
 
 def matrix_lookup(matrix, row, column)
