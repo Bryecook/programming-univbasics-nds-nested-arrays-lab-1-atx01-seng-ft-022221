b734519a605ca21ea08c35cf49d958304891547d
Brian Cook <brybcook@gmail.com>
2021-02-04 20:02:59 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -42,7 +42,10 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
-  matrix[column][row]
+  matrix= [
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE]
+    matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
